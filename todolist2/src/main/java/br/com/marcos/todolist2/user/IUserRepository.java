f8a8c0d2b0226f7e68c28af228138597e235be52
Marcos Jesus <marcos.jesus@cliqx.com.br>
2023-10-12 00:33:40 -0300

    add: repository and statuscode with ResponseEntity

---
+++ todolist2/src/main/java/br/com/marcos/todolist2/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.marcos.todolist2.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+    UserModel findByName(String name);
+}
--- todolist2/src/main/java/br/com/marcos/todolist2/user/UserController.java
+++ todolist2/src/main/java/br/com/marcos/todolist2/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.marcos.todolist2.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
-        System.out.println(userModel.getName());
-        System.out.println(userModel.getPassword());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var verifyUser = this.userRepository.findByUsername(userModel.getUsername());
+
+        var verifyNome = this.userRepository.findByName(userModel.getName());
+
+        if(verifyUser != null) {
+            System.out.println("Usuário já existe com este username");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este Username já existe!");
+        }
+
+        if(verifyNome != null) {
+            System.out.println("Este nome já está cadastrado.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este nome já existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
